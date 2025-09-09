8b75fcbd1c649682b797cfc60839290f92d8d0f3
Harshal gothriya <gothriyaharshal14@gmail.com>
2025-09-09 22:52:23 +0530

    initial commit

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <footer className = "footer">
+                 <span>
+                    © 2025 Harshal Gothriya | Employee Management App | All Rights Reserved
+                  </span> </footer>
+            </div>
+        )
+    }
+}
+
+export default FooterComponent
